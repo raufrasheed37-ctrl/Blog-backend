fd9502170ddf21d0125bf49bf7f41d7566eb2d8d
raufrasheed37-ctrl <raufrasheed37@gmail.com>
2026-05-07 17:33:22 +0100

    Wrap AuthHydrator with AuthProvider in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthHydrator from "@/components/AuthHydrator";
+import AuthProvider from "@/components/AuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +25,11 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <AuthHydrator />
-        {children}
-      </body>
+  <AuthProvider>
+    <AuthHydrator />
+    {children}
+  </AuthProvider>
+</body>
     </html>
   );
 }
