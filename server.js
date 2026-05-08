a0e91130e4b94403d0884e76aff01d8c8a34517d
David sam <davesam408@gmail.com>
2026-05-08 10:56:27 +0100

    Resolve rebase autostash conflicts across contact/auth routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 import contactRoutes from "./routes/contact.js";
+import subscribeRoutes from "./routes/subscribe.js";
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/contact", contactRoutes);
+app.use("/api/subscribe", subscribeRoutes);
 
 const PORT = process.env.PORT || 5000;
 
