78e4fdce510bd9bd1723d7acb71709eaf2677e3c
raufrasheed37-ctrl <raufrasheed37@gmail.com>
2026-08-04 18:10:20 +0100

    Fix typo in forgotPassword import statement

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, register, getMe, forgetPassword, resetPassword } from "../controllers/authController.js";
+import { login, register, getMe, forgotPassword, resetPassword } from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
-router.post("/forgot-password", forgetPassword);
+router.post("/forgot-password", forgotPassword);
 router.put("/reset-password/:token", resetPassword);
 
 export default router;
