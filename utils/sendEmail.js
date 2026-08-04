7f4014808dee5b832beb19620799a475c568c585
raufrasheed37-ctrl <raufrasheed37@gmail.com>
2026-08-04 17:58:08 +0100

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const createTransporter = () =>
   nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: 2525,
     secure: false,
     auth: {
       user: process.env.BREVO_USER,
