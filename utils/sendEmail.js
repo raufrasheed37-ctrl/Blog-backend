791a0b4ee3b35149927ea42e9dc8cb90a01bbf1d
raufrasheed37-ctrl <raufrasheed37@gmail.com>
2026-08-05 05:46:41 +0100

    Refactor sendEmail function to include replyTo option

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,27 +1,34 @@
-
 import brevo from "@getbrevo/brevo";
 
 const apiInstance = new brevo.TransactionalEmailsApi();
 
-apiInstance.setApiKey(
-  brevo.TransactionalEmailsApiApiKeys.apiKey,
-  process.env.BREVO_API_KEY
-);
+apiInstance.authentications.apiKey.apiKey =
+  process.env.BREVO_API_KEY;
 
-const sendEmail = async ({ to, subject, html }) => {
-  await apiInstance.sendTransacEmail({
-    sender: {
-      name: "Pulse Blogger",
-      email: "pulseblogger01@gmail.com"
+const sendEmail = async ({ to, subject, html, replyTo }) => {
+  const email = new brevo.SendSmtpEmail();
+
+  email.sender = {
+    name: "Pulse Blogger",
+    email: "pulseblogger01@gmail.com",
+  };
+
+  email.to = [
+    {
+      email: to,
     },
-    to: [
-      {
-        email: to,
-      },
-    ],
-    subject,
-    htmlContent: html,
-  });
+  ];
+
+  email.subject = subject;
+  email.htmlContent = html;
+
+  if (replyTo) {
+    email.replyTo = {
+      email: replyTo,
+    };
+  }
+
+  return await apiInstance.sendTransacEmail(email);
 };
 
 export default sendEmail;
